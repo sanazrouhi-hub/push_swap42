fdac75074c93cafeb5d5a522be79c03327071d91
shiva <shivakhadka0666@gmail.com>
2026-06-20 09:40:55 +0200

    changing in ss

--- operations.c
+++ operations.c
@@ -41,18 +41,38 @@ void sb(t_stack *b)
 void ss(t_stack *a, t_stack *b)
 {
     int temp;
+    int swapped;
 
+    swapped = 0;
     if (a->top && a->top->next)
     {
         temp = a->top->value;
         a->top->value = a->top->next->value;
         a->top->next->value = temp;
+        swapped = 1;
     }
     if (b->top && b->top->next)
     {
         temp = b->top->value;
         b->top->value = b->top->next->value;
         b->top->next->value = temp;
+        swapped = 1;
     }
-    write (1, "ss\n", 3);
+    if (swapped)
+        write(1, "ss\n", 3);
+}
+
+void pb(t_stack *a, t_stack *b)
+{
+    t_stack *moving;
+
+    if (!a || !a->top)
+        return;
+    moving = a->top;
+    a->top = a->top->next;
+    a->size--;
+
+    moving->next = b->top;
+    b->top = moving;
+    b->size++;
 }

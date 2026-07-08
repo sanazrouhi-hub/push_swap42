11f45f18e4930b620f36870b1f16912dd80b6ace
srouhi <sanaz.rouhip@gmail.com>
2026-07-08 11:54:31 +0200

    main

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "push_swap.h"
 #include <stdio.h>
 
-static int ft_strcmp(char *s1, char *s2)
+static int	ft_strcmp(char *s1, char *s2)
 {
 	while (*s1 && *s2 && *s1 == *s2)
 	{
@@ -11,16 +11,16 @@ static int ft_strcmp(char *s1, char *s2)
 	return (*s1 - *s2);
 }
 
-static void print_stack(t_stack *stack, char *name)
+static void	print_stack(t_stack *stack, char *name)
 {
-	t_node *current;
+	t_node	*current;
 
 	current = stack->top;
 	printf("%s: ", name);
 	if (!current)
 	{
 		printf("EMPTY\n");
-		return;
+		return ;
 	}
 	while (current)
 	{
@@ -30,46 +30,39 @@ static void print_stack(t_stack *stack, char *name)
 	printf("\n");
 }
 
-static void run_sort(t_stack *a, t_stack *b, char *flag)
+static void	run_sort(t_stack *a, t_stack *b, char *flag)
 {
-	int size;
+	int	size;
 
 	size = lst_size(a);
 	if (size <= 1)
-		return;
+		return ;
 	if (size == 2)
-	{
 		sa(a);
-		return;
-	}
-	if (size == 3)
-	{
+	else if (size == 3)
 		sort_three(&a);
-		return;
-	}
-	if (size <= 5)
-	{
+	else if (size <= 5)
 		sort_four_five(&a, &b);
-		return;
-	}
-	if (ft_strcmp(flag, "-simple") == 0)
-		big_sort(a, b);
+	else if (ft_strcmp(flag, "-simple") == 0)
+		sort_four_five(&a, &b);
 	else if (ft_strcmp(flag, "-medium") == 0)
-		big_sort(a, b);
+		chunk_sort(a, b);
 	else if (ft_strcmp(flag, "-complex") == 0)
 		radix_sort(a, b);
-	else if (ft_strcmp(flag, "-adaptive") == 0)
-		big_sort(a, b);
+	else if (ft_strcmp(flag, "-adaptive") == 0 && size <= 100)
+		chunk_sort(a, b);
+	else if (ft_strcmp(flag, "-adaptive") == 0 && size > 100)
+		radix_sort(a, b);
 	else
-		big_sort(a, b);
+		chunk_sort(a, b);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	t_stack stack_a;
-	t_stack stack_b;
-	char *flag;
-	int start;
+	t_stack	stack_a;
+	t_stack	stack_b;
+	char	*flag;
+	int		start;
 
 	if (argc < 2)
 		return (0);
@@ -79,16 +72,13 @@ int main(int argc, char **argv)
 	stack_b.size = 0;
 	flag = "-simple";
 	start = 1;
-	if (argv[1][0] == '-')
+	if (argv[1][0] == '-' && argv[1][1] != '\0')
 	{
 		flag = argv[1];
 		start = 2;
 	}
 	if (!parse_and_fill_stack(&stack_a, argc, argv, start))
-	{
-		write(2, "Error\n", 6);
-		return (1);
-	}
+		return (write(2, "Error\n", 6), 1);
 	printf("--- BEFORE ---\n");
 	print_stack(&stack_a, "A");
 	print_stack(&stack_b, "B");
@@ -98,7 +88,5 @@ int main(int argc, char **argv)
 	print_stack(&stack_a, "A");
 	print_stack(&stack_b, "B");
 	printf("-------------\n");
-	// free_stack(&(stack_a.top));
-	// free_stack(&(stack_b.top));
 	return (0);
 }
